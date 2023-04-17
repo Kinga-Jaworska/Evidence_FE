e7e4ee64e7d99b848ec0f6fe7d0eb21ec499129d
Kinga-Jaworska <violartvena@gmail.com>
2023-04-17 19:37:14 +0200

    resolve-conflicts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
@@ -14,21 +13,3 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
-=======
-import "../styles/globals.scss";
-import { Provider } from "react-redux";
-import type { AppProps } from "next/app";
-import { wrapper } from "./../store/store";
-
-function MyApp({ Component, ...rest }: AppProps) {
-  const { store, props } = wrapper.useWrappedStore(rest);
-
-  return (
-    <Provider store={store}>
-      <Component {...props.pageProps} />
-    </Provider>
-  );
-}
-
-export default wrapper.withRedux(MyApp);
->>>>>>> origin/add-form
